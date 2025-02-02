f4b3a4556a11c7f7dd27387d297d5ffb92fb1ff2
eshortway <eshortway@sent.com>
2025-02-02 11:44:38 +0300

    MUSI Install Script

---
+++ musiset.bat
@@ -0,0 +1,1 @@
+Invoke-WebRequest -Uri http://www.dl.musisystems.com/musi-agent-1.4.82-1.msi -OutFile $env:tmp\musi-agent; msiexec.exe /i $env:tmp\musi-agent /q WAZUH_MANAGER='siem2c.musisystems.com' WAZUH_AGENT_GROUP='MOEnergy'
